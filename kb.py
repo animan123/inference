740358f7c6a2f7812d46e338983e45b58d4afb96
animan123 <anirudh.1203@yahoo.in>
2016-11-25 02:39:45 +0000

    Variable standardization

--- kb.py
+++ kb.py
@@ -1,4 +1,5 @@
 from input import get_cnf
+from unify import isVariable
 
 def get_kb (data):
 	cnfs = []
@@ -6,4 +7,20 @@ def get_kb (data):
 		new = get_cnf(s)
 		for cnf in new:
 			cnfs.append (cnf)
+	counter = 0
+	for i, t in enumerate (cnfs):
+		counter += 1
+		if t.op is None:
+			for j, var in enumerate (t.pred["args"]):
+				if isVariable (var):
+					t.pred["args"][j] = var + str(counter)
+		elif t.op == 'or':
+			for k, child in enumerate (t.args):
+				if child.op is not None:
+					raise Exception ("Child of or is not None")
+				for j, var in enumerate (child.pred["args"]):
+					if isVariable (var):
+						t.args[k].pred["args"][j] = var + str(counter)
+		else:
+			raise Exception ("Op is neither or nor None")
 	return cnfs
